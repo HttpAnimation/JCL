08dcbab0f9a097de1bd6f26df16358f4cacd5e25
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-03-11 14:08:33 -0700

    Create gitter.bash

---
+++ gitter.bash
@@ -0,0 +1,10 @@
+mkdir JCL
+cd JCL
+git clone -b stable https://github.com/HttpAnimation/JCL.git
+mv JVL stable
+git clone -b gh-pages https://github.com/HttpAnimation/JCL.git
+mv JCL gh-pages
+git clone -b blank https://github.com/HttpAnimation/JCL.git
+mv JCL blank
+# git clone -b main https://github.com/HttpAnimation/JCL.git
+# mv JCL main
