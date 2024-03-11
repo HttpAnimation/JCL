403b4c21a6a9595b9abe5fd0172da55c526849fc
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-11 14:30:19 -0700

    removes old files

--- compile.sh
+++ compile.sh
@@ -12,11 +12,9 @@ mkdir -p "$build_folder"
 
 # Compile server.c and client.c into the build folder
 gcc server.c -o "$build_folder/PRS-Server"
-gcc -o "$build_folder/PRS-Client" client.c `pkg-config --cflags --libs gtk+-3.0`
 
 # Copy server.c and client.c into the build folder
 cp server.c "$build_folder/server.c"
-cp client.c "$build_folder/client.c"
 
 # Create a Version.ini file with the current date and time
 echo "$current_datetime" > "$build_folder/Version.ini"
